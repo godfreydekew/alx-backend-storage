b229f61ce1fab9ffffde0bfc2f262ad4a9d98f5c
godfreydekew <dekewgodfrey@gmail.com>
2024-07-13 08:21:15 +0000

    TAsk 3

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,7 @@
+-- sts all bands with Glam rock as 
+-- their main style, ranked by their longevity
+
+SELECT band_name, IF(split, split - formed, 2022 - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
