054abcba631eed7f7cfee548a6d5ec32da9365c8
godfreydekew <dekewgodfrey@gmail.com>
2024-07-16 02:13:47 -0700

    Task 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -6,6 +6,4 @@ inserts a new document in a collection
 
 def insert_school(mongo_collection, **kwargs):
     '''inserts a new document in a collection based on kwargs'''
-
-    for key, value in kwargs.items():
-        mongo_collection.insert_one({key: value})
+    mongo_collection.insert_one(kwargs)
