8d0bb450fc20ec1486be9154200d75beae176dd9
godfreydekew <dekewgodfrey@gmail.com>
2024-07-16 01:56:17 -0700

    Fix pycode errors

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -3,7 +3,9 @@
 List all documents in Python
 '''
 
+
 def list_all(mongo_collection):
+    '''lists all documents in a collection'''
     if mongo_collection.count_documents({}) != 0:
         return mongo_collection.find()
     return []
