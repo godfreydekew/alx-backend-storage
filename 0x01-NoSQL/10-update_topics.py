553a28c27c78ff1da6a5bf2ebc4a7b06aede834a
godfreydekew <dekewgodfrey@gmail.com>
2024-07-16 11:23:00 -0700

    Update

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -5,4 +5,7 @@ Change school topics
 
 
 def update_topics(mongo_collection, name, topics):
+    '''
+    hanges all topics of a school document based on the name
+    '''
     mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
